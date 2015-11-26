ebd82948ead2f630883e6a0b2cba6d67876377a3
RaniUma <uma.rani@dbt.co.uk>
2015-11-26 12:44:17 +0000

    comt 11

--- WebApplication3/Properties/AssemblyInfo.cs
+++ WebApplication3/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("540761ab-0c47-4bbb-8eaf-856d0fea144f")]
 
-//uma
+//uma rani
 
 // Version information for an assembly consists of the following four values:
 //
